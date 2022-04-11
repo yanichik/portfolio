9319eac612fe685aca94bd5194ad98f425f6f2be
yanichik <brunshte@gmail.com>
2022-04-10 23:13:51 -0700

    adjusted size of icons, added line at subject titles for visual effect

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -4,49 +4,22 @@ const Contact = (props) => {
 		<div className="contact" id={props.id}>
 			<div className="contact_header">
 				<span className="Nav_number">04.&ensp;</span>
-				<span className="Nav_link">C</span>ontact
+				<span className="Nav_link">G</span>et in touch
 			</div>
 			<br />
 			<div className="contact_parag">
 				<br />
-				<a href="mailto: brunshte@gmail.com" id={props.id}>
-					Send Email
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, omnis
+				ipsa fugiat ullam natus consectetur quo iure eveniet libero pariatur!
+				<a
+					className="contact_button"
+					href="mailto: brunshte@gmail.com"
+					id={props.id}
+					rel="noopener noreferrer"
+				>
+					<span className="contact_text">Get in touch!</span>
 				</a>
 				<br />
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, natus!
-				Accusantium doloribus ipsam soluta debitis iste necessitatibus,
-				repellendus ad, optio nihil itaque quos. Modi a temporibus quae error
-				velit excepturi sequi. Ipsam a dignissimos maxime tenetur tempore omnis
-				repudiandae reiciendis illum, corporis asperiores, ex ipsum quisquam!
-				Consectetur amet similique quaerat perspiciatis exercitationem impedit
-				enim doloremque illo natus. Numquam rerum nihil aliquid! Iste, totam.
-				Veniam non fugiat perspiciatis nulla. Doloremque saepe debitis sequi
-				quaerat delectus aperiam placeat? Consequatur delectus, veritatis culpa
-				voluptate necessitatibus consectetur soluta commodi debitis, aperiam vel
-				amet pariatur hic. Aperiam omnis iusto quod qui quis in incidunt
-				praesentium, commodi veritatis eum quos accusamus quaerat quisquam
-				natus, debitis saepe optio illum vel ex corrupti provident, velit
-				deserunt officiis? Voluptatibus, ab consequatur id, fugiat laudantium
-				accusamus aliquid laborum perferendis possimus itaque iusto, excepturi
-				ipsa cum modi asperiores aliquam. Illum ducimus itaque beatae quisquam,
-				eaque sit iste earum. Explicabo esse ducimus neque voluptate cupiditate,
-				pariatur quod sint perferendis? Animi quis expedita, similique molestias
-				accusamus voluptas consequuntur nostrum necessitatibus at minus impedit?
-				Minus illum eum repellendus dolor explicabo nobis hic neque vitae unde
-				veritatis dolore id labore, facilis nostrum quia possimus. Velit
-				corrupti consequuntur ipsam, libero minima ex qui eius quod natus
-				distinctio repellat error dolorem eligendi rem aliquid provident
-				corporis quasi perferendis ullam architecto optio. Corporis dicta ipsam
-				voluptatum perspiciatis voluptatem dolorem ratione aut explicabo? Autem
-				sit dolore sapiente inventore totam quae, earum odit quas illum vero
-				facilis aut quaerat ullam perspiciatis molestias perferendis quasi
-				voluptas labore, accusamus iusto pariatur, sequi cumque enim! Dolor
-				harum vel at magnam nam repellendus vitae quisquam quod cupiditate
-				quasi. Corrupti veritatis sunt ipsa qui eligendi sapiente nobis
-				reprehenderit obcaecati, facere officiis voluptatum nemo quas impedit
-				repellendus voluptate. Quae autem facilis id est assumenda. Optio
-				impedit blanditiis dignissimos asperiores. Nesciunt, quidem rem totam
-				autem deserunt officiis?
 			</div>
 		</div>
 	);
